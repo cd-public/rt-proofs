dc2742f624371a2e8fd5b8d9a072b71fa6c8e34c
Felipe Cerqueira <felipec@mpi-sws.org>
2015-03-05 14:52:57 +0100

    Latest changes

--- job-logic.v
+++ job-logic.v
@@ -1,49 +1,12 @@
 Require Import Coq.Lists.List.
 Require Import Coq.Arith.EqNat.
 
-(* A job represents an execution requirement *)
-Record job : Type :=
-  { job_id: nat; (* identifier *)
-    job_number: nat; (* sequence number *)
-    job_arrival: nat;
-    job_cost: nat;
-    job_deadline: nat
-  }.
-
-(* Restrictions on job parameters *)
-Record well_defined_job (j: job) : Prop :=
-  { job_cost_positive: job_cost j > 0;
-    job_cost_le_deadline: job_cost j <= job_deadline j;
-    job_deadline_positive: job_deadline j > 0
-  }.
-
-(* Sporadic Task Model (assumes integer time) *)
-Record sporadic_task : Type :=
-  { st_id: nat;
-    st_cost: nat;
-    st_period: nat;
-    st_deadline: nat;
-    st_offset: nat;
-    st_arrival: nat -> nat
-  }.
-
-(* Restrictions on the task parameters *)
-Record well_defined_task (task: sporadic_task) : Prop :=
-  { st_cost_positive: st_cost task > 0;
-    st_cost_le_period: st_cost task <= st_period task;
-    st_cost_le_deadline: st_cost task <= st_deadline task;
-    st_period_positive: st_period task > 0;
-    st_deadline_positive: st_deadline task > 0;
-    st_arrival_start: st_arrival task 0 = st_offset task;
-    st_arrival_valid: forall i, st_arrival task i <= st_arrival task (i+1) + st_period task
-  }.
-
-Definition taskset := list sporadic_task.
-
-Definition valid_taskset (ts: taskset) : Prop := List.Forall well_defined_task ts.
+Require Import job.
 
 Definition processor := nat.
 Definition time := nat.
+
+Definition valid_taskset (ts: taskset) : Prop := List.Forall well_defined_task ts.
 
 Record schedule_state : Type :=
   {
@@ -69,7 +32,7 @@ Record valid_schedule_state (s: schedule_state) : Prop :=
     forall j cpu, scheduled j cpu s -> List.In j (active_jobs s)
   }.
 
-Definition sched_algorithm := schedule_state -> schedule_state.
+Definition sched_algorithm := schedule_state -> Prop.
 
 Fixpoint list_none (n:nat) : list (option job) :=
   match n with
