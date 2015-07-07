09e3ea730ec39dd4ffd5d046fd60f905b8cb4c63
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-07 21:11:17 +0200

    Use better names

--- priority.v
+++ priority.v
@@ -1,14 +1,15 @@
 Require Import List Relations Classical Arith Vbase extralib ExtraRelations task job task_arrival helper schedule.
 Set Implicit Arguments.
 
-Hypothesis same_id_same_task : (* Necessary to break the ties in priority *)
-  forall tsk1 tsk2, task_id tsk1 = task_id tsk2 -> tsk1 = tsk2.
+(* Task ids are assumed to uniquely identify a task. Necessary to break ties in priority. *)
+Hypothesis same_id_same_task : forall tsk1 tsk2 (EQid: task_id tsk1 = task_id tsk2), tsk1 = tsk2. 
+Definition break_ties (tsk1 tsk2: sporadic_task) := task_id tsk1 < task_id tsk2.
 
 Definition task_hp_relation := sporadic_task -> sporadic_task -> Prop.
 Definition job_hp_relation := job -> job -> Prop.
-Definition sched_job_hp_relation := schedule -> time -> job -> job -> Prop.
+Definition sched_job_hp_relation := schedule -> time -> job_hp_relation.
 
-Definition higher_priority (sched: schedule) (t: time) (hp_rel: job_hp_relation) :=
+Definition valid_jldp_policy (sched: schedule) (t: time) (hp_rel: job_hp_relation) :=
   << hpIrr: irreflexive hp_rel >> /\
   << hpAsym: asymmetric hp_rel >> /\
   << hpTrans: transitive _ hp_rel >> /\
@@ -19,11 +20,11 @@ Definition higher_priority (sched: schedule) (t: time) (hp_rel: job_hp_relation)
 
 (* Observations/TODO *)
 (* 1) A higher-priority order serves to compare jobs of different tasks, since
-      jobs of the same task already have precedence. Making the relation total
-      over any job would only complicate the specification (a lot of
+      jobs of the same task already have precedence. Making the relation non-strict
+      and total would only complicate the specification (a lot of
       "if j1 and j2 are from the same task" when defining RM and EDF). *)
 
-Definition task_higher_priority (task_hp_rel: task_hp_relation) :=
+Definition valid_fp_policy (task_hp_rel: task_hp_relation) :=
   << hpIrr: irreflexive task_hp_rel >> /\
   << hpAntisym: asymmetric task_hp_rel >> /\
   << hpTrans: transitive _ task_hp_rel >> /\
@@ -32,19 +33,16 @@ Definition task_higher_priority (task_hp_rel: task_hp_relation) :=
 (* Rate-Monotonic and Deadline-Monotonic priority order *)
 Definition RM (tsk1 tsk2: sporadic_task) :=
   << LTper: task_period tsk1 < task_period tsk2 >> \/
-  << TIE: (task_period tsk1 = task_period tsk2 /\ task_id tsk1 < task_id tsk2) >>.
+  << TIE: (task_period tsk1 = task_period tsk2 /\ break_ties tsk1 tsk2) >>.
 
 Definition DM (tsk1 tsk2: sporadic_task) :=
   << LTper: task_deadline tsk1 < task_deadline tsk2 >> \/
-  << TIE: (task_deadline tsk1 = task_deadline tsk2 /\ task_id tsk1 < task_id tsk2) >>.
+  << TIE: (task_deadline tsk1 = task_deadline tsk2 /\ break_ties tsk1 tsk2) >>.
 
-Lemma rm_valid_fp_policy : task_higher_priority RM.
+Lemma rm_is_valid : valid_fp_policy RM.
 Proof.
-  unfold task_higher_priority, irreflexive, asymmetric, transitive, RM;
-    repeat (split; try red); ins.
-  by des; intuition.
-  by des; intuition.
-  by des; try rewrite TIE in *; eauto using lt_trans, eq_trans.
+  unfold valid_fp_policy, irreflexive, asymmetric, transitive, RM, break_ties;
+  repeat (split; try red); ins; try by (des; intuition).
   destruct (lt_eq_lt_dec (task_period tsk1) (task_period tsk2)) as [DEC2 | LTper];
   [destruct DEC2 as [LTper| EQper] |].
     by left; left.
@@ -56,13 +54,10 @@ Proof.
     by right; left.
 Qed.
 
-Lemma dm_valid_fp_policy : task_higher_priority DM.
+Lemma dm_is_valid : valid_fp_policy DM.
 Proof.
-  unfold task_higher_priority, irreflexive, asymmetric, transitive, DM;
-    repeat (split; try red); ins.
-  by des; intuition.
-  by des; intuition.
-  by des; try rewrite TIE in *; eauto using lt_trans, eq_trans.
+  unfold valid_fp_policy, irreflexive, asymmetric, transitive, DM, break_ties;
+  repeat (split; try red); ins; try by (des; intuition).
   destruct (lt_eq_lt_dec (task_deadline tsk1) (task_deadline tsk2)) as [DEC2 | LTdl];
   [destruct DEC2 as [LTdl| EQper] |].
     by left; left.
@@ -75,21 +70,21 @@ Proof.
 Qed.
 
 (* Relate task priority with job priority *)
-Definition fixed_priority (hp: sched_job_hp_relation) (task_hp: task_hp_relation) :=
+Definition convert_fp_jldp (task_hp: task_hp_relation) (hp: sched_job_hp_relation) :=
   forall sched t jhigh jlow,
     hp sched t jhigh jlow <->
       (<< NEQtsk: (job_task jhigh) <> (job_task jlow) >> /\
        << HPtsk: task_hp (job_task jhigh) (job_task jlow) >>).
 
-Lemma fp_valid_policy :
+Lemma valid_fp_is_valid_jldp :
   forall ts sched t hp task_hp
          (ARRts: ts_arrival_sequence ts sched) (* All jobs come from taskset *)
-         (VALIDthp: task_higher_priority task_hp)
-         (FP: fixed_priority hp task_hp), higher_priority sched t (hp sched t).
+         (FP: valid_fp_policy task_hp)
+         (CONV: convert_fp_jldp task_hp hp), valid_jldp_policy sched t (hp sched t).
 Proof.
-  unfold task_higher_priority, fixed_priority, higher_priority, irreflexive,
+  unfold valid_fp_policy, valid_jldp_policy, convert_fp_jldp, irreflexive,
   asymmetric, transitive, ts_arrival_sequence; repeat (split; try red); ins; des;
-  rewrite FP in *; try rewrite FP in *; des; repeat (split; try red); eauto.
+  rewrite CONV in *; try rewrite CONV in *; des; repeat (split; try red); eauto.
     by unfold not; intro EQ; rewrite EQ in *; eauto.
     {
       apply ARRts in ARRj1; apply ARRts in ARRj2.
@@ -99,7 +94,7 @@ Proof.
 Qed.
 
 (* Job-level fixed priority *)
-Definition job_level_fixed_priority (hp: sched_job_hp_relation) :=
+Definition jlfp_policy (hp: sched_job_hp_relation) :=
   forall sched j1 j2 t t' (HP: hp sched t j1 j2), hp sched t' j1 j2.
 
 Definition EDF (sched: schedule) (t: time) (j1 j2: job) :=
@@ -107,24 +102,23 @@ Definition EDF (sched: schedule) (t: time) (j1 j2: job) :=
                 << ARR2: arrives_at sched j2 r2 >> /\
                 (<< LTper: r1 + job_deadline j1 < r2 + job_deadline j2 >> \/
                  <<  TIE: (r1 + job_deadline j1 = r2 + job_deadline j2 /\
-                           task_id (job_task j1) < task_id (job_task j2)) >>).
+                           break_ties (job_task j1) (job_task j2)) >>).
 
-Lemma edf_jlfp : job_level_fixed_priority EDF. 
+Lemma edf_jlfp : jlfp_policy EDF. 
 Proof.
-  by unfold job_level_fixed_priority, EDF.
+  by unfold jlfp_policy, EDF.
 Qed.
 
-Lemma fp_implies_jlfp :
-  forall hp task_hp (FP: fixed_priority hp task_hp) (VALIDthp: task_higher_priority task_hp),
-    job_level_fixed_priority hp.
+Lemma fp_is_jlfp :
+  forall hp task_hp (CONV: convert_fp_jldp task_hp hp), jlfp_policy hp.
 Proof.
-  unfold task_higher_priority, fixed_priority, job_level_fixed_priority; ins; des.
-  rewrite FP in *; eauto.
+  unfold jlfp_policy, valid_fp_policy, convert_fp_jldp; ins; des.
+  rewrite CONV in *; eauto.
 Qed.
 
-Lemma edf_valid_policy : forall sched t, higher_priority sched t (EDF sched t).
+Lemma edf_valid_policy : forall sched t, valid_jldp_policy sched t (EDF sched t).
 Proof.
-  unfold higher_priority, EDF, irreflexive, asymmetric, transitive;
+  unfold valid_jldp_policy, EDF, irreflexive, asymmetric, transitive, break_ties;
   repeat (split; try red); ins.
     by des; [assert (r1 = r2) by eauto using no_multiple_arrivals; subst|]; intuition.
     by des; assert (r0 = r2); assert (r1 = r3); eauto using no_multiple_arrivals; by intuition.
@@ -156,8 +150,8 @@ Proof.
 Qed.
 
 Lemma fp_schedule_independent :
-  forall hp tsk_hp (FP: fixed_priority hp tsk_hp), schedule_independent hp.
+  forall hp tsk_hp (CONV: convert_fp_jldp tsk_hp hp), schedule_independent hp.
 Proof.
-  unfold schedule_independent, fixed_priority, RM; repeat (split; try red);
-  ins; des; rewrite FP in *; eauto.
+  unfold schedule_independent, convert_fp_jldp, RM; repeat (split; try red);
+  ins; des; rewrite CONV in *; eauto.
 Qed.
