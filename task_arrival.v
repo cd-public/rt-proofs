92af5e31cba36b893969ba7fb12d88892d3f090d
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-08 18:27:16 +0200

    Add finite list of arrivals

--- task_arrival.v
+++ task_arrival.v
@@ -1,7 +1,4 @@
-Require Import task.
-Require Import job.
-Require Import schedule.
-Require Import Coq.Lists.List.
+Require Import List Vbase task job schedule.
 
 (* Whether the arrival sequence of a schedule is induced by a task set *)
 Definition ts_arrival_sequence (ts: taskset) (sched: schedule) :=
@@ -10,28 +7,35 @@ Definition ts_arrival_sequence (ts: taskset) (sched: schedule) :=
 (* Since the number of tasks is finite, and each task does not
    spawn more than one job per time instant, the number of jobs
    released in a bounded interval is finite. *)
-Lemma finite_arrival_sequence (ts: taskset) (sched: schedule) :
-  forall (ARRts: ts_arrival_sequence ts sched)
-         j arr (ARR: arrives_at sched j arr),
-    exists (l: list job), In j l <-> arrives_at sched j arr.
-
+Definition arrival_list (sched: schedule) (l: list job) (t: time) :=
+  forall j, << IN: In j l >> <->
+            << ARRIVED: (arrived sched j t) >>.
+    
+Lemma ts_finite_arrival_sequence:
+  forall ts sched (ARRts: ts_arrival_sequence ts sched) (t: time),
+    exists (l: list job), arrival_list sched l t.
+Proof.
+Admitted.
 
 (* Sporadic arrival times for every task in a taskset.
-   We enforce -> only because the first arrival may be at any point. *)
+   We use only '->' because the first arrival may be at any point. *)
 Definition periodic_task_model (ts: taskset) (sched: schedule) :=
-  forall (ARRts: ts_arrival_sequence ts sched) j t (ARRj: arrives_at sched j t),
-  exists (j': job) (t': time), job_task j' = job_task j /\
-                               arrives_at sched j' t' /\
-                               t' = t + task_period (job_task j).
+  << ARRts: ts_arrival_sequence ts sched >> /\
+  forall j arr (ARRj: arrives_at sched j arr),
+    exists (j': job) (arr': time), job_task j' = job_task j /\
+                                   arrives_at sched j' arr' /\
+                                   arr' = arr + task_period (job_task j).
 
 (* Periodic arrival times *)
 Definition sporadic_task_model (ts: taskset) (sched: schedule) :=
-  forall (ARRts: ts_arrival_sequence ts sched) j t (ARRj: arrives_at sched j t),
-  exists (j': job) (t': time), job_task j' = job_task j /\
-                               arrives_at sched j' t' /\
-                               t' >= t + task_period (job_task j).
+  << ARRts: ts_arrival_sequence ts sched >> /\
+  forall j arr (ARRj: arrives_at sched j arr),
+    exists (j': job) (arr': time), job_task j' = job_task j /\
+                                   arrives_at sched j' arr' /\
+                                   arr' >= arr + task_period (job_task j).
 
 (* Synchronous release at time t *)
 Definition sync_release (ts: taskset) (sched: schedule) (t: time) :=
+  << ARRts: ts_arrival_sequence ts sched >> /\
   forall (tsk: sporadic_task) (IN: In tsk ts),
     exists (j: job), job_task j = tsk /\ arrives_at sched j t.
