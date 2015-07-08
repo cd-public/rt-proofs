3d708c1fe231925abdbf521ca9919867584d76ec
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-08 18:46:05 +0200

    Fix definition of identmp

--- identmp.v
+++ identmp.v
@@ -5,13 +5,17 @@ Definition job_mapping := job -> processor_id -> time -> Prop.
 (* Identical multiprocessor platform *)
 Definition ident_mp (num_cpus: nat) (hp: sched_job_hp_relation)
                     (mapped: job_mapping) (sched: schedule) :=
-  (* The mapping has a finite number of cpus: [1, num_cpus) *)
+  (* The mapping has a finite positive number of cpus: [0, num_cpus) *)
   << mp_cpus_nonzero: num_cpus > 0 >> /\
-  << mp_num_cpus: forall j cpu t, mapped j cpu t <-> cpu < num_cpus >> /\
+  << mp_num_cpus: forall j cpu t, mapped t j cpu -> cpu < num_cpus >> /\
 
-  (* Job is scheduled iff it is mapped to a processor*)
-  << mp_mapping: forall j cpu t, scheduled sched j t <-> mapped j cpu t >> /\
+  (* Job is scheduled iff it is mapped to some processor*)
+  << mp_mapping: forall j t, scheduled sched j t <-> exists cpu, mapped j cpu t >> /\
 
+  (* Non-parallelism restrictions (mapping must be an injective function) *)
+  << mp_mapping_fun: forall j cpu cpu' t, mapped j cpu t /\ mapped j cpu' t -> cpu = cpu' >> /\
+  << mp_mapping_inj: forall j j' cpu t, mapped j cpu t /\ mapped j' cpu t -> j = j'>> /\
+  
   (* A job receives at most 1 unit of service *)
   << mp_max_service: forall j t, service_at sched j t <= 1 >> /\
 
@@ -21,9 +25,6 @@ Definition ident_mp (num_cpus: nat) (hp: sched_job_hp_relation)
       (exists (j0: job), earlier_job sched j0 jlow /\ pending sched j0 t) \/
       (forall cpu (MAXcpu: cpu < num_cpus),
         exists jhigh, hp sched t jhigh jlow /\ mapped jhigh cpu t) >>.
-
-
-
 
 (*Require Export Coq.Structures.OrderedTypeEx.*)
 
