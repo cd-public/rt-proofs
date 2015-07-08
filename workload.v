1c53210d84f1c225df719bb6de7c20c332f82c3a
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-08 18:27:28 +0200

    Define workload

---
+++ workload.v
@@ -0,0 +1,30 @@
+Require Import Arith List Vbase job task schedule identmp priority helper task_arrival.
+
+Section WorkloadBound.
+
+(* The workload is defined as the total service received by jobs of
+   a specific task in the interval [t,t'+1). To allow summing over the
+   jobs released in the interval we assume that a list of arrived jobs
+   (along with a proof that this is the right list) is passed by
+   parameter. *)
+Definition workload (sched: schedule) (ts: taskset) (tsk: sporadic_task) (t t': time)
+                    (l: list job) (ARRlist: arrival_list sched l t') : nat :=
+  fold_left plus (map
+                    (fun j => service sched j t' - service sched j t) (* Check if off by 1*)
+                    (filter (fun j => beq_nat (job_deadline j) 1) (* FIX: job_task j = tsk *)
+                  l)
+                 ) 0.
+
+Definition W (tsk: sporadic_task) (delta: time) := 100. (* FIX: replace by formula later *)
+
+Lemma workload_bound :
+  forall ts sched (ARRts: ts_arrival_sequence ts sched)
+         tsk (IN: In tsk ts) j (JOB: job_task j = tsk)
+         arr (ARRj: arrives_at sched j arr)
+         l (ARRlist: arrival_list sched l (arr + job_deadline j)),
+    (workload sched ts tsk arr (arr + job_deadline j) l ARRlist) < W tsk (job_deadline j).
+Proof.
+  unfold arrival_list; ins; des.
+Admitted.
+
+End WorkloadBound.
