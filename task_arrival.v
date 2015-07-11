b018c539ed3eb3544f2dc7cf94a234f53a21c6de
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-11 20:49:46 +0200

    Prove finite arrival sequence

--- task_arrival.v
+++ task_arrival.v
@@ -4,46 +4,84 @@ Require Import List Classical Vbase task job schedule.
 Definition ts_arrival_sequence (ts: taskset) (sched: schedule) :=
   forall j t (ARR: arrives_at sched j t), In (job_task j) ts.
 
+Definition task_job_unique (sched: schedule) :=
+  forall t j1 j2 (EQtsk: job_task j1 = job_task j2)
+         (ARR1: arrives_at sched j1 t) (ARR2: arrives_at sched j2 t),
+    j1 = j2.
+
 (* Since the number of tasks is finite, and each task does not
    spawn more than one job per time instant, the number of jobs
    released in a bounded interval is finite. *)
 Definition arrival_list (sched: schedule) (l: list job) (t: time) :=
-  forall j, << IN: In j l >> <->
-            << ARRIVED: (arrived sched j t) >>.
+  forall j, In j l <-> arrived sched j t.
 
-(*Lemma released_jobs_at_time:
-  forall ts sched t (ARRts: ts_arrival_sequence ts sched),
+Lemma released_jobs_at:
+  forall ts sched (ARRts: ts_arrival_sequence ts sched)
+         (UNIQUE: task_job_unique sched) t,
     exists (l: list job),
       forall j, In j l <-> arrives_at sched j t.
 Proof.
-  unfold ts_arrival_sequence; ins.
-  (*cut (exists l, forall j, exists tsk, In j l <-> (arrives_at sched) j t /\ job_task j = tsk).
-    by ins; des; exists l; ins; rewrite H with (tsk := job_task j) in *; split; ins; des.
-    induction ts.
-      by exists nil; split; ins; des; eauto.
+  unfold task_job_unique, ts_arrival_sequence; ins.
+  cut (exists l, forall j, In j l <-> arrives_at sched j t /\
+                                      In (job_task j) ts).
+    intro LISTtsk; des; exists l; split.
+      by intro INj; apply LISTtsk in INj; des.
+      by rewrite LISTtsk; split; eauto.
 
-    by rewrite H with (tsk := job_task j) in *; des.
-      apply ARRts in H0.
-  destruct ts. exists nil; split; ins; eauto.
-  destruct (classic (exists j, arrives_at sched j t /\ job_task j = s)); des.
-  admit.*)
-  induction ts; ins.
-    exists nil; split; ins; eauto.
-    
-
-    destruct (classic (exists j, arrives_at sched j t )); des.
-
-      by eauto.
-*)      
+  specialize (UNIQUE t); clear ARRts.
+  induction ts as [| tsk1]; ins; des.
+    (* Base Case *)
+    by exists nil; split; ins; des; trivial.
+    (* Inductive step *)
+    destruct (classic (exists j, arrives_at sched j t /\
+                       job_task j = tsk1)) as [ARRtsk1 | NOTARRtsk1]; des.
+    {
+      exists (j :: l); ins.
+      split; ins; des; subst.
+        by split; eauto.
+        by rewrite IHts in H; des; eauto.
+        by left; eauto.
+        by right; rewrite IHts; split; eauto.
+    }
+    {
+      exists l; split; ins; des; subst.
+        by rewrite IHts in H; des; eauto.
+        by exfalso; apply NOTARRtsk1; exists j; eauto.
+        by rewrite IHts; eauto.
+    }
+Qed.
+         
 Lemma ts_finite_arrival_sequence:
-  forall ts sched (ARRts: ts_arrival_sequence ts sched) (t: time),
+  forall ts sched (ARRts: ts_arrival_sequence ts sched)
+         (UNIQUE: task_job_unique sched) (t: time),
     exists (l: list job), arrival_list sched l t.
 Proof.
-  unfold ts_arrival_sequence, arrival_list; ins.
-  induction t.
-  exists (map (arrives_at sched t))
-  unfold arrived.
-Admitted.
+  unfold ts_arrival_sequence, arrival_list, arrived; ins.
+  assert (ARR:= released_jobs_at ts sched ARRts UNIQUE).
+  induction t as [| t ARRbefore].
+  {
+    specialize (ARR 0); des.
+    exists l; ins; split; intro H; des.
+      by exists 0; split; [trivial |]; apply ARR; eauto.
+      by red; assert (t_0 = 0); [omega |]; subst; apply ARR.
+  }
+  {
+    destruct ARRbefore as [l_before ARRbefore].
+    specialize (ARR (S t)); destruct ARR as [l_cur ARRcur].
+    exists (l_before ++ l_cur); ins; split.
+    {
+      intro IN; apply in_app_or in IN; des.
+        by rewrite ARRbefore in IN; des; exists t_0; split; eauto.
+        by exists (S t); split; [trivial |]; rewrite <- ARRcur.
+    }
+    {
+      intro BEFORE; des.
+      apply in_or_app; inversion BEFORE; subst.
+        by right; rewrite ARRcur.
+        by left; rewrite ARRbefore; exists t_0; split.
+    }
+  }
+Qed.
 
 (* Sporadic arrival times for every task in a taskset.
    We use only '->' because the first arrival may be at any point. *)
