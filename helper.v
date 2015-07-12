7b2a5cea0065adb5c990d5d031832b183a3d46ee
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-12 16:50:48 +0200

    Defnire more lemmas about sum_list

--- helper.v
+++ helper.v
@@ -1,47 +1,90 @@
-Require Import List Arith Vbase.
+Require Import List Arith Classical Vbase.
 
-Lemma nat_seq_nth_In : forall n len (LT: n < len), In n (seq 0 len).
+Lemma no_elements_nil :
+  forall (A: Type) (l: list A) (NOELEM: forall x, In x l -> False), l = nil.
 Proof.
-  ins.
-  assert (NTH := seq_nth).
-  generalize LT; apply NTH with (start := 0) (d := len) in LT; simpl in LT; ins.
-  assert (NTHIn := nth_In).
-  specialize (NTHIn nat n (seq 0 len) len).
-  rewrite (seq_length len 0), LT in NTHIn.
-  eauto.
-Qed.
-  
-Lemma In_singleton_list :
-    forall (A: Type) (x: A) l n, In (Some x) l -> n < 1 -> length l = 1 -> nth n l None = Some x.
-Proof.
-  intros.
-  destruct n; [|inversion H0; inversion H3].
-  destruct l; [inversion H1|].
-  destruct l; [|inversion H1].
-  destruct H; [|contradiction].
-  auto.
+  ins; destruct l; trivial.
+  by exfalso; apply (NOELEM a); left.
 Qed.
 
-Lemma element_in_list : forall (A: Type) (x: A) (l: list (option A)) (n: nat),
-                            nth n l None = Some x -> In (Some x) l.
+Lemma length_comm :
+  forall (A: Type) (l1 l2: list A) (SAME: forall x, In x l1 <-> In x l2),
+    length l1 = length l2.
 Proof.
-    intros. assert (H1 := nth_in_or_default).
-    specialize (H1 (option A) n l None).
-    inversion H1. rewrite <- H. apply H0.
-    rewrite H in H0. inversion H0.
-Qed.
-
-Lemma element_in_list_no_overflow :
-    forall (A: Type) (x: A) (l: list (option A)) (n: nat),
-        nth n l None = Some x -> n < length l.
-Proof.
+  ins; remember (length l1) as len.
 Admitted.
 
-Definition least_nat (n: nat) (P: nat -> Prop) : Prop :=
-    P n /\ forall (n': nat), P n' -> n <= n'.
+Lemma length_nil :
+  forall (A: Type) (l: list A), length l = 0 <-> l = nil.
+Proof.
+  split; intro H; [| by rewrite H; simpl].
+  destruct l; [by trivial | by simpl in *; inversion H].
+Qed.
 
-Definition greatest_nat (n: nat) (P: nat -> Prop) : Prop :=
-    P n /\ forall (n': nat), P n' -> n' <= n.
+Lemma list_nonempty :
+  forall (l: list nat), l <> nil <-> exists a, In a l.
+Proof.
+  split; intro H; des.
+    destruct l.
+      by trivial.
+      by exists n; left.
+    by unfold not; ins; subst; trivial.
+Qed.
 
-Lemma not_gt_le : forall n m : nat, ~ n > m -> n <= m.
-Proof. by ins; omega. Qed.
+Lemma split_inclusion_helper :
+  forall (A: Type) (l0 l1 l2 l3: list A) (a: A)
+         (OR: In a l0 \/ In a l1)
+         (ALL: forall x : A, In x (l0 ++ a :: l1) <-> In x (l2 ++ a :: l3)),
+    In a l2 \/ In a l3.
+Proof.
+  ins. remember (length (l0 ++ a :: l1)) as len. 
+  generalize dependent l1. generalize dependent l0.
+  induction len; ins.
+  {
+    symmetry in Heqlen. apply length_nil in Heqlen.
+    symmetry in Heqlen.
+    exfalso. assert (NOTNIL := app_cons_not_nil).
+    specialize (NOTNIL A l0 l1 a). intuition.
+  }
+  (* NOT ABLE TO PROVE THIS *)
+Admitted.
+
+Lemma split_inclusion :
+  forall (A: Type) (l0 l1 l2 l3: list A) (a: A),
+    (forall x : A, In x (l0 ++ l1) <-> In x (l2 ++ l3)) <->
+    (forall x : A, In x (l0 ++ a :: l1) <-> In x (l2 ++ a :: l3)).
+Proof.
+  split; ins.
+  {
+    rewrite 2 in_app_iff in *.
+    simpl in *.
+    cut (In x l0 \/ In x l1 <-> In x l2 \/ In x l3); ins; [by tauto|].
+    specialize (H x).
+    by rewrite 2 in_app_iff in *.
+  }
+  {
+    split; ins.
+    {
+      rewrite in_app_iff in *.
+      assert (In x (l2 ++ a :: l3)).
+        rewrite <- H; rewrite in_app_iff.
+        destruct H0; [by left | by right; right].
+      rewrite in_app_iff in H1.
+      destruct H1; simpl in H1; [by left|].
+        destruct H1; subst.
+          by apply (split_inclusion_helper A l0 l1); eauto.
+          by right.
+    }
+    {
+      rewrite in_app_iff in *.
+      assert (In x (l0 ++ a :: l1)).
+        rewrite H; rewrite in_app_iff.
+        destruct H0; [by left | by right; right].
+      rewrite in_app_iff in H1.
+      destruct H1; simpl in H1; [by left|].
+        destruct H1; subst.
+          by apply (split_inclusion_helper A l2 l3); ins; split; rewrite H.
+          by right.
+    }
+  }
+Qed.
--- sum.v
+++ sum.v
@@ -1,8 +1,8 @@
-Require Import Vbase List Classical.
+Require Import Vbase List Classical helper.
 
 Definition list_sum (l: list nat) := fold_right plus 0 l.
 
-Lemma sum_max :
+Lemma sum_list_max_bound :
   forall (l: list nat) (max: nat)
          (MAX: forall x (IN: In x l), x <= max),
     list_sum l <= (length l) * max.
@@ -23,24 +23,50 @@ Proof.
       }
 Qed.
 
-Lemma no_elements_nil :
-  forall {X: Type} (l: list X) (NOELEM: forall x, In x l -> False), l = nil.
+Lemma sum_list_app :
+  forall (l1 l2: list nat),
+    list_sum (l1 ++ l2) = list_sum l1 + list_sum l2.
 Proof.
-  ins; destruct l; trivial.
-  by exfalso; apply (NOELEM x); left.
+  induction l1; [by trivial|]; ins.
+    destruct l2.
+      by simpl; rewrite app_nil_r; rewrite <- plus_n_O.
+      by rewrite IHl1; simpl; omega. 
 Qed.
 
-Lemma len_commutative :
-  forall (l1 l2: list nat) (SAME: forall x, In x l1 <-> In x l2),
-    length l1 = length l2.
-Proof.
-  ins.
-Admitted.
-
-
-Lemma sum_commutative :
+Lemma sum_list_comm :
   forall (l1 l2: list nat) (SAME: forall x, In x l1 <-> In x l2),
     list_sum l1 = list_sum l2.
 Proof.
+  ins; remember (length l1) as len.
+  assert (Heqlen2: len = length l2).
+    by rewrite <- length_comm with (l1 := l1).
+  generalize dependent l2.
+  generalize dependent l1.
+  generalize dependent len. 
+  induction len; ins; simpl in *.
+    (* Base case : empty list *)
+    by symmetry in Heqlen, Heqlen2; rewrite length_nil in *; subst; simpl.
+    (* Inductive Step *)
+    assert (exists a, In a l1); des.
+      by apply list_nonempty; unfold not; ins; subst; simpl in *.
+    assert (In a l2). by rewrite <- SAME.
+    apply in_split in H0; apply in_split in H; des; subst.
+    rewrite 2 sum_list_app; simpl.
+    rewrite app_length in *. simpl in *.
+    rewrite <- plus_n_Sm in *.
+    inversion Heqlen. inversion Heqlen2.
+    rewrite <- app_length in *.
+    rewrite <- split_inclusion in SAME.
+    cut (list_sum l4 + list_sum l5 = list_sum l0 + list_sum l3); ins; [by omega|].
+    rewrite <- 2 sum_list_app; eauto using IHlen.
+Qed.
+
+Lemma sum_list_partition :
+  forall (l l1 l2: list nat)
+         f (PART: partition f l = (l1, l2)),
+    list_sum l = list_sum l1 + list_sum l2.
+Proof.
   ins.
+  rewrite <- sum_list_app.
+  apply sum_list_comm.
 Admitted.
