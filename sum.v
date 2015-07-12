2d639a613b4da67214c55b36f132189b56b8e763
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-12 03:13:07 +0200

    Fix names

--- sum.v
+++ sum.v
@@ -30,7 +30,7 @@ Proof.
   by exfalso; apply (NOELEM x); left.
 Qed.
 
-Lemma sum_commutative2 :
+Lemma len_commutative :
   forall (l1 l2: list nat) (SAME: forall x, In x l1 <-> In x l2),
     length l1 = length l2.
 Proof.
@@ -42,10 +42,5 @@ Lemma sum_commutative :
   forall (l1 l2: list nat) (SAME: forall x, In x l1 <-> In x l2),
     list_sum l1 = list_sum l2.
 Proof.
-  ins; remember (length l1) as len1; remember (length l2) as len2.
-  generalize dependent l2. generalize dependent l1.
-  induction len1; ins.
-  admit.
-  destruct l1; [by intuition|].
-  destruct l2.
+  ins.
 Admitted.
