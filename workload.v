93ff02d4ccb48e244727084bea60ca1f8da5bf7a
Felipe Cerqueira <felipec@mpi-sws.org>
2015-07-14 11:22:14 +0200

    Fix definition of workload

--- workload.v
+++ workload.v
@@ -37,7 +37,7 @@ Definition carried_in (sched: schedule) (tsk: sporadic_task) (t: time) (j: job) 
   << TSKj: job_task j = tsk >> /\
   << EARLIER: arrived sched j (t - 1) >> /\
   << INTERF: exists t', t' >= t /\
-                        scheduled sched j t' >>.
+                        scheduled sched j t' >>. (* scheduled or pending? *)
 
 Lemma workload_bound :
   forall ts sched (ARRts: ts_arrival_sequence ts sched)
@@ -46,65 +46,67 @@ Lemma workload_bound :
          arr (ARRj: arrives_at sched j arr)
          released_jobs (ARRlist: arrival_list sched released_jobs (arr + job_deadline j - 1))
          (SCHED: task_misses_no_deadlines sched ts tsk),
-    (workload sched ts tsk arr (arr + job_deadline j) released_jobs) <= W tsk (job_deadline j).
+    (workload sched ts tsk arr (arr + job_deadline j) released_jobs) <= W tsk (arr + job_deadline j).
 Proof.
   intros.
   unfold workload.
-  destruct (classic (arr = 0)) as [| NZEROarr]; try subst arr.
-    (* j arrives at time 0: no carried-in job *)
-    admit.
-    (* j arrives at later time *)
-    destruct (classic (exists j_0, carried_in sched tsk arr j_0)); des.
-    {
-      (* Carried-in job j_0 *)
-      assert (INj_0: j_0 \in released_jobs).
-      {
-        (*unfold carried_in, arrival_list, arrived in *; rewrite ARRlist; des.
-        by exists t_0; split; [ssromega|].
-        -------------------------------BROKEN because \in <> In*)
-        admit.
-      }
-      (* At most one carried-in job *)
-      assert (UNIQcarry:
-                forall j1 j2 (IN1: j1 \in released_jobs)
-                       (IN2: j2 \in released_jobs)
-                       (C1: carried_in sched tsk arr j1)
-                       (C2: carried_in sched tsk arr j2), j1 = j2).
-      {
-        ins. admit.
-      }
 
-      rewrite big_seq_cond.
-      rewrite (bigID (beq_job j_0)); simpl.
-      unfold W.
-      apply leq_add.
-      {
-        (* Prove that the service of the carried-in job <= min()*)
-        admit.
+  (* There exists at most one carried-in job *)
+  assert (UNIQcarry:
+            forall j1 j2 (IN1: j1 \in released_jobs)
+                   (IN2: j2 \in released_jobs)
+                   (C1: carried_in sched tsk arr j1)
+                   (C2: carried_in sched tsk arr j2), j1 = j2).
+  {
+    ins. admit.
+  }
+
+  destruct (classic (exists j_0, carried_in sched tsk arr j_0)); des.
+  {
+    (* Carried-in job j_0 *)
+    assert (INj_0: j_0 \in released_jobs).
+    {
+      (*unfold carried_in, arrival_list, arrived in *; rewrite ARRlist; des.
+      by exists t_0; split; [ssromega|].
+      -------------------------------BROKEN because \in <> In*)
+      admit.
+    }
+
+    rewrite big_seq_cond.
+    rewrite (bigID (beq_job j_0)); simpl.
+    unfold W.
+    apply leq_add.
+    {
+      (* Prove that the service of the carried-in job <= min()*)
+      admit.
       }
       {
         (* Prove that service of other jobs <= task_cost *)      
         apply leq_trans with
           (n := \sum_(i <- released_jobs | (i \in released_jobs) &&
                  job_of tsk i && ~~beq_job j_0 i) task_cost tsk).
-        apply leq_sum; intro j_i; ins.
-        unfold service_during, andb, negb in *; desf.
-        rewrite leq_subLR.
-        (*easy to prove, since j_i does not execute for more than WCET *)
-        admit.
+        {
+          apply leq_sum; intro j_i; ins.
+          unfold service_during, andb, negb in *; desf.
+          rewrite leq_subLR.
+          rewrite <- addn0; rewrite addnC.
+          rewrite leq_add; ins.
+          (*easy to prove, since j_i does not execute for more than WCET *)
+          admit.
+        }
+        {
+          rewrite big_const_seq, iter_addn, addn0, mulnC.
+          rewrite leq_mul2r.
+          unfold orb; desf.
+          unfold max_jobs.
 
-        rewrite big_const_seq, iter_addn.
-        rewrite addn0, mulnC.
-        rewrite leq_mul2r.
-        unfold orb; desf.
-        unfold max_jobs.
-
-        (* Prove that the number of non-carry-in jobs is at most n_k *)
-        admit.
+          (* Prove that the number of non-carry-in jobs is at most n_k *)
+          admit.
+        }
       }
     }
     {
-      (* No carried-in job *)
+      (* There exists no carried-in job *)
       admit.
     }
 Qed.
