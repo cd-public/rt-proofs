ccc2bd9c490348a53e503a58df2f807823b4d423
Felipe Cerqueira <felipec@mpi-sws.org>
2015-09-07 17:32:40 +0200

    Minor changes

--- BertognaResponseTimeDefs.v
+++ BertognaResponseTimeDefs.v
@@ -73,30 +73,43 @@ Module ResponseTimeAnalysis.
     Variable tsk: sporadic_task.
     Hypothesis task_in_ts: tsk \in ts.
 
-    Variable R_other: sporadic_task -> time.
-    
     Definition no_deadline_is_missed_by_tsk (tsk: sporadic_task) :=
       task_misses_no_deadline job_cost job_deadline job_task rate sched tsk.
 
-    Definition response_time_bounded_by :=
+    Definition is_response_time_bound (tsk: sporadic_task) :=
       is_response_time_bound_of_task job_cost job_task tsk rate sched.
+    
+    Variable R_other: sporadic_task -> time.
 
     Section UnderFPScheduling.
 
       Variable higher_eq_priority: fp_policy.
-      
+
+      Hypothesis response_time_of_interfering_tasks_is_known:
+        forall tsk_other,
+          tsk_other \in ts -> (* --> ugly, convert this to some predicate *)
+          tsk_other != tsk ->
+          higher_eq_priority tsk_other tsk ->
+          is_response_time_bound tsk_other (R_other tsk_other).
+
       (* Bertogna and Cirinei's response-time bound recurrence *)
       Definition response_time_recurrence_fp R :=
         R <= task_cost tsk + div_floor
-                             (total_interference_fp ts tsk R_other R higher_eq_priority)
-                             num_cpus.
+                               (total_interference_fp ts tsk R_other R higher_eq_priority)
+                               num_cpus.
 
+      Variable R: time.
+
+      Hypothesis response_time_recurrence_holds:
+        response_time_recurrence_fp R.
+
+      Hypothesis response_time_no_larger_than_deadline:
+        R <= task_deadline tsk.
+      
       Theorem bertogna_cirinei_response_time_bound_fp :
-        forall R,
-          response_time_recurrence_fp R ->
-          R <= task_deadline tsk ->
-          response_time_bounded_by R.
+        is_response_time_bound tsk R.
       Proof.
+        unfold response_time_recurrence_fp; ins.      
       Admitted.
 
     End UnderFPScheduling.
@@ -109,11 +122,16 @@ Module ResponseTimeAnalysis.
                              (total_interference_jlfp ts tsk R_other R)
                              num_cpus.
 
-      Theorem bertogna_cirinei_response_time_bound :
-        forall R,
-          response_time_recurrence_jlfp R ->
-          R <= task_deadline tsk ->
-          response_time_bounded_by R.
+      Variable R: time.
+
+      Hypothesis response_time_recurrence_holds:
+        response_time_recurrence_jlfp R.
+
+      Hypothesis response_time_no_larger_than_deadline:
+        R <= task_deadline tsk.
+
+      Theorem bertogna_cirinei_response_time_bound_jlfp :
+        is_response_time_bound tsk R.
       Proof.
       Admitted.
       
